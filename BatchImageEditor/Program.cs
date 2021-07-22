33e000ad6cdd3aa2cd1d4a65fb43202f028419b8
Tomáš Boleslav <boleslavtomas@gmail.com>
2021-07-22 19:21:53 +0200

    Creating new MainForm

--- BatchImageEditor/Program.cs
+++ BatchImageEditor/Program.cs
@@ -14,7 +14,7 @@ namespace BatchImageEditor
 			Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new AppForm());
+			Application.Run(new MainForm());
 		}
 	}
 }
