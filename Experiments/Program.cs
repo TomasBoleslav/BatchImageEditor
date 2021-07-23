07f073b4e223d4917a37bce6aba4428bbd52879c
Tomáš Boleslav <boleslavtomas@gmail.com>
2021-07-23 22:26:31 +0200

    Add client project for experiments with library

---
+++ Experiments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Experiments
+{
+	class Program
+	{
+		static Action<string> Act;
+
+		static void Write(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		static void Main(string[] args)
+		{
+			Act = Write;
+			Act?.Invoke("Hello!");
+		}
+	}
+}
