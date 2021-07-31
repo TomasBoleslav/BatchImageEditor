6a9d0052c4b75e7c69aea848687f6c8caf45d2d1
Tomáš Boleslav <boleslavtomas@gmail.com>
2021-07-31 22:31:10 +0200

    Added filter list design

---
+++ BatchImageEditor/FilterList.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace BatchImageEditor
+{
+	partial class FilterList
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.SuspendLayout();
+			// 
+			// button3
+			// 
+			this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button3.Location = new System.Drawing.Point(257, 85);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(100, 35);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Edit";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.Location = new System.Drawing.Point(257, 44);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 35);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Remove";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(257, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 35);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(3, 3);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(248, 488);
+			this.checkedListBox1.TabIndex = 5;
+			// 
+			// FilterList
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.checkedListBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button3);
+			this.Name = "FilterList";
+			this.Size = new System.Drawing.Size(360, 507);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+	}
+}
