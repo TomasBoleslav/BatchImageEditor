20bf6bf335a9f83789759f33c82c9a99e686bf62
Tomáš Boleslav <boleslavtomas@gmail.com>
2021-07-31 23:41:39 +0200

    Modified design of filter edit form

--- BatchImageEditor/FilterEditForm.Designer.cs
+++ BatchImageEditor/FilterEditForm.Designer.cs
@@ -31,7 +31,8 @@ namespace BatchImageEditor
 		{
 			this._previewGroup = new System.Windows.Forms.GroupBox();
 			this.imagePreview1 = new BatchImageEditor.ImagePreview();
-			this._settingsGroup = new System.Windows.Forms.GroupBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
 			this._previewGroup.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -58,23 +59,31 @@ namespace BatchImageEditor
 			this.imagePreview1.Size = new System.Drawing.Size(498, 414);
 			this.imagePreview1.TabIndex = 2;
 			// 
-			// _settingsGroup
+			// button1
 			// 
-			this._settingsGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this._settingsGroup.Location = new System.Drawing.Point(528, 12);
-			this._settingsGroup.Name = "_settingsGroup";
-			this._settingsGroup.Size = new System.Drawing.Size(258, 446);
-			this._settingsGroup.TabIndex = 2;
-			this._settingsGroup.TabStop = false;
-			this._settingsGroup.Text = "Settings";
+			this.button1.Location = new System.Drawing.Point(600, 423);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(90, 35);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(696, 423);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(90, 35);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Reset";
+			this.button2.UseVisualStyleBackColor = true;
 			// 
 			// FilterEditForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(798, 470);
-			this.Controls.Add(this._settingsGroup);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this._previewGroup);
 			this.Name = "FilterEditForm";
 			this.Text = "FilterEditForm";
@@ -86,6 +95,7 @@ namespace BatchImageEditor
 		#endregion
 		private System.Windows.Forms.GroupBox _previewGroup;
 		private ImagePreview imagePreview1;
-		private System.Windows.Forms.GroupBox _settingsGroup;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
 	}
 }
--- BatchImageEditor/IFilterSettings.cs
+++ BatchImageEditor/IFilterSettings.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using ImageFilters;
 
 namespace BatchImageEditor
 {
 	internal interface IFilterSettings
 	{
+		event EventHandler Changed;
+
 		void Reset();
 
 		IEnumerable<IImageFilter> CreateFilters();
