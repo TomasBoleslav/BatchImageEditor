87d6cae6944f342ca8303f70641d6dd965719ae3
Tomáš Boleslav <boleslavtomas@gmail.com>
2021-08-03 00:05:33 +0200

    Added filter list to edit scene design

--- BatchImageEditor/EditScene.Designer.cs
+++ BatchImageEditor/EditScene.Designer.cs
@@ -29,90 +29,46 @@ namespace BatchImageEditor
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.filtersGroup = new System.Windows.Forms.GroupBox();
-			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.filtersGroup.SuspendLayout();
+			this.filterList1 = new BatchImageEditor.FilterList();
+			this._filtersGroup = new System.Windows.Forms.GroupBox();
+			this._filtersGroup.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// filtersGroup
+			// filterList1
 			// 
-			this.filtersGroup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-			this.filtersGroup.Controls.Add(this.button3);
-			this.filtersGroup.Controls.Add(this.button2);
-			this.filtersGroup.Controls.Add(this.button1);
-			this.filtersGroup.Controls.Add(this.checkedListBox1);
-			this.filtersGroup.Location = new System.Drawing.Point(18, 18);
-			this.filtersGroup.Name = "filtersGroup";
-			this.filtersGroup.Padding = new System.Windows.Forms.Padding(6);
-			this.filtersGroup.Size = new System.Drawing.Size(319, 540);
-			this.filtersGroup.TabIndex = 0;
-			this.filtersGroup.TabStop = false;
-			this.filtersGroup.Text = "Filters";
+			this.filterList1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.filterList1.InputBitmap = null;
+			this.filterList1.Location = new System.Drawing.Point(3, 23);
+			this.filterList1.Name = "filterList1";
+			this.filterList1.Size = new System.Drawing.Size(349, 514);
+			this.filterList1.TabIndex = 0;
 			// 
-			// checkedListBox1
+			// _filtersGroup
 			// 
-			this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.checkedListBox1.FormattingEnabled = true;
-			this.checkedListBox1.Location = new System.Drawing.Point(9, 29);
-			this.checkedListBox1.Name = "checkedListBox1";
-			this.checkedListBox1.Size = new System.Drawing.Size(200, 488);
-			this.checkedListBox1.TabIndex = 1;
-			// 
-			// button1
-			// 
-			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.button1.Location = new System.Drawing.Point(215, 29);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(95, 30);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "Add";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.button2.Location = new System.Drawing.Point(215, 65);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(95, 30);
-			this.button2.TabIndex = 3;
-			this.button2.Text = "Remove";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.button3.Location = new System.Drawing.Point(215, 101);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(95, 30);
-			this.button3.TabIndex = 4;
-			this.button3.Text = "Edit";
-			this.button3.UseVisualStyleBackColor = true;
+			this._filtersGroup.Controls.Add(this.filterList1);
+			this._filtersGroup.Location = new System.Drawing.Point(18, 18);
+			this._filtersGroup.Name = "_filtersGroup";
+			this._filtersGroup.Size = new System.Drawing.Size(355, 540);
+			this._filtersGroup.TabIndex = 1;
+			this._filtersGroup.TabStop = false;
+			this._filtersGroup.Text = "Filters";
 			// 
 			// EditScene
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.filtersGroup);
+			this.Controls.Add(this._filtersGroup);
 			this.Name = "EditScene";
 			this.Padding = new System.Windows.Forms.Padding(15);
 			this.Size = new System.Drawing.Size(1011, 576);
-			this.filtersGroup.ResumeLayout(false);
+			this._filtersGroup.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.GroupBox filtersGroup;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private FilterList filterList1;
+		private System.Windows.Forms.GroupBox _filtersGroup;
 	}
 }
