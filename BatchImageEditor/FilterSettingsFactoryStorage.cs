35901c95e9417e18a5bb57bf6982d9fe096da6fd
Tomáš Boleslav <boleslavtomas@gmail.com>
2021-08-18 15:55:27 +0200

    Renaming

--- BatchImageEditor/FilterSettingsFactoryStorage.cs
+++ BatchImageEditor/FilterSettingsFactoryStorage.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BatchImageEditor
 {
@@ -14,19 +11,19 @@ namespace BatchImageEditor
 		public void Add<T>(IFactory<T> factory) where T : FilterSettingsBase
 		{
 			string name = FilterSettingsNames.GetName(typeof(T));
-			settingNamesToFactories.Add(name, factory);
+			_settingNamesToFactories.Add(name, factory);
 		}
 
 		public IFactory<FilterSettingsBase> GetFactory(string name)
 		{
 			IFactory<FilterSettingsBase> factory;
-			if (settingNamesToFactories.TryGetValue(name, out factory))
+			if (_settingNamesToFactories.TryGetValue(name, out factory))
 			{
 				return factory;
 			}
 			throw new ArgumentException("There is no factory with the given name.");
 		}
 
-		private readonly Dictionary<string, IFactory<FilterSettingsBase>> settingNamesToFactories = new();
+		private readonly Dictionary<string, IFactory<FilterSettingsBase>> _settingNamesToFactories = new();
 	}
 }
